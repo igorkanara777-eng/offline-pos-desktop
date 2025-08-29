25a5b1197bd7d5b2701cc62519c60fa68e31f106
igorkanara777-eng <igorkanara777@gmail.com>
2025-08-29 23:03:05 +0300

    Update preload.ts

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,2 +1,2 @@
-import { contextBridge, ipcRenderer } from 'electron';
-contextBridge.exposeInMainWorld('electron', { invoke: (channel, ...args) => ipcRenderer.invoke(channel, ...args) });
+// можно оставить пустым или добавить безопасные API
+export {}
