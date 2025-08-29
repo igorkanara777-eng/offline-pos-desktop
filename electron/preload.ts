759cb39a61af584ba77274a488abbf85893f7403
igorkanara777-eng <igorkanara777@gmail.com>
2025-08-30 01:38:20 +0300

    Update preload.ts

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,2 +1,15 @@
-// можно оставить пустым или добавить безопасные API
-export {}
+import { contextBridge, ipcRenderer } from "electron";
+import type { Product } from "./db";
+
+contextBridge.exposeInMainWorld("api", {
+  app: {
+    version: () => ipcRenderer.invoke("app:version"),
+  },
+  products: {
+    list: (q?: string) => ipcRenderer.invoke("products:list", q) as Promise<Product[]>,
+    create: (p: Omit<Product, "id">) => ipcRenderer.invoke("products:create", p) as Promise<Product>,
+    update: (p: Product) => ipcRenderer.invoke("products:update", p) as Promise<Product>,
+    remove: (id: number) => ipcRenderer.invoke("products:remove", id) as Promise<void>,
+    adjust: (id: number, delta: number) => ipcRenderer.invoke("products:adjust", { id, delta }) as Promise<Product>,
+  },
+});
