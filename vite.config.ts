3cc39930c89f436fcf751975b2b681637bca598b
igorkanara777-eng <igorkanara777@gmail.com>
2025-08-30 15:16:58 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 // vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // КРИТИЧНО: для Electron нужен относительный base, иначе белый экран
-  base: './',
+  base: './', // критично для loadFile(...)
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-  }
-})
+  },
+});
